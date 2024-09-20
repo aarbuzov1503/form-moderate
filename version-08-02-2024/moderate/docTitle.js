3a49bc820daada1d49446109a05607765c299426
Александр <109166493+aarbuzov1503@users.noreply.github.com>
2024-09-20 16:01:30 +0500

    Delete version-08-02-2024 directory

--- version-08-02-2024/moderate/docTitle.js
+++
@@ -1,7 +0,0 @@
-let docTitle = document.title;
-window.addEventListener("blur", () => {
-    document.title = "Ты куда пропал?";
-})
-window.addEventListener("focus", () => {
-    document.title = docTitle;
-})
